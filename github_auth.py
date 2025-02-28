fc00d77abcce0ed1ef125854ecb8fdba4f97ab34
wdmcnamee17 <wmcnamee@montgomerycollege.edu>
2025-02-28 14:18:33 -0700

    Last updat: GitHub API script that fetches user info and repos

--- github_auth.py
+++ github_auth.py
@@ -1,5 +1,21 @@
 import requests
 import passwords
-url = "https://api.github.com/user"
-response = requests.get(url, auth=(passwords.USERNAME, passwords.PASSWORD))
-print(response.json())
+
+user_url = "https://api.github.com/user"
+user_response = requests.get(user_url, auth=(passwords.USERNAME, passwords.PASSWORD))
+
+if user_response.status_code == 200:
+    print("User Info:", user_response.json())
+else:
+    print("Failed to fetch user info:", user_response.status_code, user_response.text)
+
+repo_url = "https://api.github.com/user/repos"
+repo_response = requests.get(repo_url, auth=(passwords.USERNAME, passwords.PASSWORD))
+
+if repo_response.status_code == 200:
+    repos = repo_response.json()
+    print("\nGitHub Repositories:")
+    for repo in repos:
+        print(f"- {repo['name']} (URL: {repo['html_url']})")
+else:
+    print("Failed to retrieve repositories:", repo_response.status_code, repo_response.text)
